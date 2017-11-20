40c9314c864417b41dd0b811ad56c288325468e7
Danielle H Gamble <dg12181@srcs.loc>
2017-11-20 12:02:13 -0800

    js file update

--- team_website_functions.js
+++ team_website_functions.js
@@ -1,1 +1,13 @@
-hello
+ < h1 > Today 's Date</h1> <
+ script >
+     function dateMessage();
+ var today = new Date();
+ var dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+ var day = today.getDay();
+ var MonthArray = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+ var month = today.getMonth();
+ var date = today.getDate();
+ var year = today.getFullYear();
+
+ document.write(dayArray[day], ", ", MonthArray[month], " ", [date], ", ", [year]); <
+ /script>
