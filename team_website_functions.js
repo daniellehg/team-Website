5a92839c91ddca89102bb47c5dc6a616eca3ffa9
Michelle E Gamble <mg79570@srcs.loc>
2017-11-27 11:05:46 -0800

    Greeting 1

--- team_website_functions.js
+++ team_website_functions.js
@@ -11,3 +11,7 @@
 
  return (dayArray[day], ", ", MonthArray[month], " ", [date], ", ", [year]); <
  /script>
+
+ var greetings = ['Howdy!', 'Greetings!', 'Hola!', 'Bonjour!', 'Shalom!', 'hi'];
+        var rand = greetings[Math.floor(Math.random() * greetings.length)];
+        document.write(rand);
