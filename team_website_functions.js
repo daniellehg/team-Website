0ac48046aca0c7283c29558604b2f2fceba998ba
Michelle E Gamble <mg79570@srcs.loc>
2017-11-27 12:18:18 -0800

    greeting js and html- doesn't work

--- team_website_functions.js
+++ team_website_functions.js
@@ -1,11 +1,6 @@
- <html> Today 's Date</html>
-<<<<<<< HEAD
-
- function dateMessage();
-=======
+ < h1 > Today 's Date</h1> <
  script >
      function dateMessage();
->>>>>>> origin/master
  var today = new Date();
  var dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
  var day = today.getDay();
@@ -14,38 +9,11 @@
  var date = today.getDate();
  var year = today.getFullYear();
 
- return dayArray[day] + ", ", MonthArray[month] + " ", [date] + ", ", [year]
+ return (dayArray[day], ", ", MonthArray[month], " ", [date], ", ", [year]); <
+ /script>
 
-<<<<<<< HEAD
- var greetings = ['Howdy!', 'Greetings!', 'Hola!', 'Bonjour!', 'Shalom!', 'hi'];
- var rand = greetings[Math.floor(Math.random() * greetings.length)];
- document.write(rand);
-=======
-<<<<<<< HEAD
-     function birthdaycountdown(date){
-     return date
+ function randomGreetings() {
+     var greetings = ['Howdy!', 'Greetings!', 'Hola!', 'Bonjour!', 'Shalom!', 'hi'];
+     var randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+     return randomGreeting;
  }
-      var today= new Date();
-      var birthday = new Date();
-      var days = Math.floor((birthday.getTime() - today.getTime() / 86400000);
-
-      var today = new Date();
-      var lastday = new Date();
-      var hours = Math.floor((birthday.getTime() - today.getTime()) / 3600000);
-
-      var today = new Date();
-      var lastday = new Date();
-      var minutes = Math.floor((birthday.getTime() - today.getTime()) / 60000);
-
-
-
-
-
-
-
-=======
-var greetings = ['Howdy!', 'Greetings!', 'Hola!', 'Bonjour!', 'Shalom!', 'hi'];
-        var rand = greetings[Math.floor(Math.random() * greetings.length)];
-        document.write(rand);
->>>>>>> origin/master
->>>>>>> origin/master
