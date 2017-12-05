b1e9268e5e29cd4fa17f3f5b20d1664ed9b96334
Danielle H Gamble <dg12181@srcs.loc>
2017-12-05 11:37:30 -0800

    commit

--- team_website_functions.js
+++ team_website_functions.js
@@ -1,43 +1,42 @@
-<<<<<<< HEAD
-  Today's Date
 
 
- function dateMessage();
+function dateMessage();
+var today = new Date();
+var dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+var day = today.getDay();
+var MonthArray = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var month = today.getMonth();
+var date = today.getDate();
+var year = today.getFullYear();
+if (date == 0 || date == 20 || date == 30) {
 
-=======
- < h1 > Today 's Date</h1>
->>>>>>> origin/master
-     function dateMessage();
- var today = new Date();
- var dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
- var day = today.getDay();
- var MonthArray = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
- var month = today.getMonth();
- var date = today.getDate();
- var year = today.getFullYear();
+    DateSuffix = "st"
+}
 
- return dayArray[day], ", ", MonthArray[month], " ", [date], ", ", [year]
+return dayArray[day], ", ", MonthArray[month], " ", [date], ", ", [year]
 
- function randomGreetings() {
-     var greetings = ['Howdy!', 'Greetings!', 'Hola!', 'Bonjour!', 'Shalom!', 'hi'];
-     var randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
-     return randomGreeting;
- }
-<<<<<<< HEAD
-     function birthdaycountdown(d) {
-     var today = new Date ();
-     var bday = new Date(" ");
-     bdate.setFullYear(today.getFullYear());
-    var untilBD = today-bdate;
-     if (untilBD < 0){
-         var c = new Date(year + 1, month, day)
-         }
+function randomGreetings() {
+    var greetings = ['Howdy!', 'Greetings!', 'Hola!', 'Bonjour!', 'Shalom!', 'hi'];
+    var randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+    return randomGreeting;
+} <<
+<< << < HEAD
+
+function birthdaycountdown(d) {
+    var today = new Date();
+    var bday = new Date(" ");
+    bdate.setFullYear(today.getFullYear());
+    var untilBD = today - bdate;
+    if (untilBD < 0) {
+        var c = new Date(year + 1, month, day)
+    }
 
 
 
-=======
-var greetings = ['Howdy!', 'Greetings!', 'Hola!', 'Bonjour!', 'Shalom!', 'hi'];
-        var rand = greetings[Math.floor(Math.random() * greetings.length)];
-        document.write(rand);
-=======
->>>>>>> origin/master
+    ===
+    === =
+    var greetings = ['Howdy!', 'Greetings!', 'Hola!', 'Bonjour!', 'Shalom!', 'hi'];
+    var rand = greetings[Math.floor(Math.random() * greetings.length)];
+    document.write(rand); ===
+    === = >>>
+    >>> > origin / master
