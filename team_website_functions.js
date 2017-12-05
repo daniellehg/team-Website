8a5f8119f653e123edd7443ea1e7580805e1876d
Danielle H Gamble <dg12181@srcs.loc>
2017-12-05 11:58:33 -0800

    ok

--- team_website_functions.js
+++ team_website_functions.js
@@ -35,20 +35,6 @@ function birthdaycountdown(d) {
 }
 =======
 
- function dateMessage();
-
-     function dateMessage();
- var today = new Date();
- var dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
- var day = today.getDay();
- var MonthArray = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
- var month = today.getMonth();
- var date = today.getDate();
- var year = today.getFullYear();
->>>>>>> origin/master
-
- return dayArray[day], ", ", MonthArray[month], " ", [date], ", ", [year]; <
- /script>
 
  function greeting(d) {
     var hour = d.getHours();
