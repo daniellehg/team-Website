642414df843dab2b05ceb03eb442e09a336ff312
Michelle E Gamble <mg79570@srcs.loc>
2017-12-05 11:58:23 -0800

    greeting morning

--- team_website_functions.js
+++ team_website_functions.js
@@ -1,33 +1,39 @@
+function dateMessage();
 
+function dateMessage();
+var today = new Date();
+var dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+var day = today.getDay();
+var MonthArray = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var month = today.getMonth();
+var date = today.getDate();
+var year = today.getFullYear();
 
+return dayArray[day], ", ", MonthArray[month], " ", [date], ", ", [year]; <
+/script>
 
- function dateMessage();
-
-     function dateMessage();
- var today = new Date();
- var dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
- var day = today.getDay();
- var MonthArray = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
- var month = today.getMonth();
- var date = today.getDate();
- var year = today.getFullYear();
-
- return dayArray[day], ", ", MonthArray[month], " ", [date], ", ", [year]; <
- /script>
-
- function greeting(d) {
+function greeting(d) {
     var hour = d.getHours();
     var day = d.getDay();
-    if (day == 6 || day == 0) {
-
+    if (hour < 12) {
+        if (day == 0 || day == 6) {
+            return "Go back to sleep, it's the weekend!";
+        } else if (day == 1) {
+            return "Ugh, it's monday morning!";
+        } else {
+            return "Good Morning!"
+        }
+    }
+    if (hour > 12 && hour < 17) {
+        if ()
     }
 }
-     function birthdaycountdown(d) {
-     var today = new Date ();
-     var bday = new Date(" ");
-     bdate.setFullYear(today.getFullYear());
-    var untilBD = today-bdate;
-     if (untilBD < 0){
-         var c = new Date(year + 1, month, day)
-         }
 
+function birthdaycountdown(d) {
+    var today = new Date();
+    var bday = new Date(" ");
+    bdate.setFullYear(today.getFullYear());
+    var untilBD = today - bdate;
+    if (untilBD < 0) {
+        var c = new Date(year + 1, month, day)
+    }
