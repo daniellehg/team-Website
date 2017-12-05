7379b08025f2462aace6f03b96f1b1e273f33f31
Michelle E Gamble <mg79570@srcs.loc>
2017-12-05 12:04:26 -0800

    greeting morning

--- team_website_functions.js
+++ team_website_functions.js
@@ -1,5 +1,6 @@
+function dateMessage();
 
-function dateMessage(){
+function dateMessage();
 var today = new Date();
 var dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 var day = today.getDay();
@@ -7,20 +8,25 @@ var MonthArray = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'August
 var month = today.getMonth();
 var date = today.getDate();
 var year = today.getFullYear();
-if (date == 0 || date == 20 || date == 30) {
 
-    DateSuffix = "st";
+return dayArray[day], ", ", MonthArray[month], " ", [date], ", ", [year];
+
+function greeting(d) {
+    var hour = d.getHours();
+    var day = d.getDay();
+    if (hour < 12) {
+        if (day == 0 || day == 6) {
+            return "Go back to sleep, it's the weekend!";
+        } else if (day == 1) {
+            return "Ugh, it's monday morning!";
+        } else {
+            return "Good Morning!"
+        }
+    }
+    if (hour > 12 && hour < 17) {
+        if (day == )
+    }
 }
-
-return dayArray[day], ", ", MonthArray[month], " ", [date], ", ", [year]
-}
-
-function randomGreetings() {
-    var greetings = ['Howdy!', 'Greetings!', 'Hola!', 'Bonjour!', 'Shalom!', 'hi'];
-    var randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
-    return randomGreeting;
-} <<
-<< << < HEAD
 
 function birthdaycountdown(d) {
     var today = new Date();
@@ -30,23 +36,3 @@ function birthdaycountdown(d) {
     if (untilBD < 0) {
         var c = new Date(year + 1, month, day)
     }
-}
-=======
-
-
- function greeting(d) {
-    var hour = d.getHours();
-    var day = d.getDay();
-    if (day == 6 || day == 0) {
-
-    }
-}
-     function birthdaycountdown(d) {
-     var today = new Date ();
-     var bday = new Date(" ");
-     bdate.setFullYear(today.getFullYear());
-    var untilBD = today-bdate;
-     if (untilBD < 0){
-         var c = new Date(year + 1, month, day)
-         }
-
